d4fb63731b07e87bfb69e2dac55ce595c66390f8
xuyongsheng <xuyongsheng@nibirutech.com>
2018-11-16 11:11:14 +0800

    feat: add jaeger tracing plugin

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const initJaegerTracer = require('jaeger-client').initTracer
+const {opentracing, initTracer} = require('jaeger-client')
 const fp = require('fastify-plugin')
 const Ajv = require('ajv')
 const jaegerSchema = require('./jaeger-schema')
 const logger = require('simple-json-logger')
 
-function FastifyJaeger (fastify, params, next) {
+function fastifyJaeger (fastify, params, next) {
   const baseConfig = {
     reporter: {logSpans: true}
   }
   const baseOptions = {logger}
   const {config, options} = params
   const ajv = new Ajv()
-  const jSchema = ajv.compile(jaegerSchema)
-  const valid = jSchema.validate(config)
+  const validateJaegerSchema = ajv.compile(jaegerSchema)
+  const valid = validateJaegerSchema(config)
   if (!valid) throw new Error('Jaeger config is not valid')
 
-  fastify.tracer = initJaegerTracer({...baseConfig, ...config}, {...baseOptions, ...options})
+  const tracer = initTracer({...baseConfig, ...config}, {...baseOptions, ...options})
+  fastify.decorate('tracer', tracer)
+  fastify.decorate('opentracing', opentracing)
+  fastify.addHook('onClose', (_, done) => {
+    tracer.close(done)
+  })
   next()
 }
 
-module.exports = fp(FastifyJaeger)
+module.exports = fp(fastifyJaeger)
