a91f4ce067f677ce026037fa669506298945a17d
chux0519 <chuxdesign@hotmail.com>
2018-11-16 20:10:15 +0800

    fix: catch the potential error from jaeger-client

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-const {opentracing, initTracer} = require('jaeger-client')
+const { opentracing, initTracer } = require('jaeger-client')
 const fp = require('fastify-plugin')
-const {Validator} = require('jsonschema')
+const { Validator } = require('jsonschema')
 const jaegerSchema = require('./jaeger-schema')
 const logger = require('simple-json-logger')
 
 function fastifyJaeger (fastify, params, next) {
-  const baseConfig = {
-    reporter: {logSpans: true}
-  }
-  const baseOptions = {logger}
-  const {config, options} = params
-  const v = new Validator()
-  v.validate(config, jaegerSchema, {
-    throwError: true
-  })
+  try {
+    const baseConfig = {
+      reporter: { logSpans: true }
+    }
+    const baseOptions = { logger }
+    const { config, options } = params
+    const v = new Validator()
+    v.validate(config, jaegerSchema, {
+      throwError: true
+    })
 
-  const tracer = initTracer({...baseConfig, ...config}, {...baseOptions, ...options})
-  fastify.decorate('tracer', tracer)
-  fastify.decorate('opentracing', opentracing)
-  fastify.addHook('onClose', (_, done) => {
-    tracer.close(done)
-  })
-  next()
+    const tracer = initTracer({ ...baseConfig, ...config }, { ...baseOptions, ...options })
+    fastify.decorate('tracer', tracer)
+    fastify.decorate('opentracing', opentracing)
+    fastify.addHook('onClose', (_, done) => {
+      tracer.close(done)
+    })
+    next()
+  } catch (e) {
+    next(new Error(e.message))
+  }
 }
 
 module.exports = fp(fastifyJaeger, {
